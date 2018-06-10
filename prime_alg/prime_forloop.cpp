674ee3073fb3ae880cf7507ae29fa41c4e1032dc
wombat22 <wombocombo222@web.de>
2018-06-10 22:02:08 +0200

    prime folder

---
+++ prime_alg/prime_forloop.cpp
@@ -0,0 +1,20 @@
+#include "hallo1.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+
+
+using namespace std;
+
+
+
+
+//int main(){
+  //  cout<< "hallo nummer zwei";
+//}
+
+
+
